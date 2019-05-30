620bc63d12c8827e80b34d09a31d6d0098b23b23
raravilov <raravilov@gmail.com>
2019-05-30 03:05:31 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import csv
+from pprint import pprint
+
+def inflation_view(request):
+    data_list = []
+    template_name = 'inflation.html'
+    i = 0
+    with open('inflation_russia.csv', encoding='utf8', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for rows in reader:
+            i+=1
+            if i!=1:
+                buf_rows = [float(x) for x in rows if (x != '') and (x != rows[0])]
+                buf_rows.insert(0,rows[0])
+                data_list.append(buf_rows)
+
+    head_list = ['Год','Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек', 'Суммарная']
+
+    # чтение csv-файла и заполнение контекста
+    context =  {'head_list': head_list,
+                'data_list': data_list}
+
+    return render(request, template_name,
+                  context)
